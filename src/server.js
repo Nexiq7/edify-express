ca0db46edb18ac6dd03cded5b612f06db4522a04
Paulo <zeronflash@gmail.com>
2025-08-31 19:41:53 +0200

    added callback url with orign

--- src/server.js
+++ src/server.js
@@ -32,7 +32,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
      clientID: process.env.GOOGLE_CLIENT_ID,
      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-     callbackURL: "/api/v1/auth/google/callback",
+     callbackURL: `${process.env.ORIGIN}/api/v1/auth/google/callback`,
 }, async (accessToken, refreshToken, profile, done) => {
      try {
           let user = await prisma.user.findUnique({ where: { googleId: profile.id } });
